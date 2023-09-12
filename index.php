8aa81f614207f4147bbbe14498659419d8859aa8
cbappi <abdulwahedrr@gmail.com>
2023-09-12 22:47:08 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,62 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Basic Calculator</title>
+    <style>
+  <?php include "styles.css" ?>
+</style>
+</head>
+<body>
+    <div class="container">
+        <h2>Simple Calculator</h2>
+        <form method="post" action="">
+            <input type="number" name="num1" placeholder="Enter first number" required><br>
+            <input type="number" name="num2" placeholder="Enter second number" required><br>
+            <select name="operation">
+                <option value="add">addition</option>
+                <option value="subtract">subtraction</option>
+                <option value="multiply">multiplication</option>
+                <option value="divide">division</option>
+            </select><br>
+          
+            <button type="submit">Calculate</button>
+        </form>
+        <div id="result">
+            <?php
+            if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                $num1 = $_POST["num1"];
+                $num2 = $_POST["num2"];
+                $operation = $_POST["operation"];
+
+                switch ($operation) {
+                    case "add":
+                        $result = $num1 + $num2;
+                        echo "Result: $result";
+                        break;
+                    case "subtract":
+                        $result = $num1 - $num2;
+                        echo "Result: $result";
+                        break;
+                    case "multiply":
+                        $result = $num1 * $num2;
+                        echo "Result: $result";
+                        break;
+                    case "divide":
+                        if ($num2 != 0) {
+                            $result = $num1 / $num2;
+                            echo "Result: $result";
+                        } else {
+                            echo "Cannot divide by zero.";
+                        }
+                        break;
+             
+                }
+            }
+            ?>
+        </div>
+    </div>
+</body>
+</html>
